bbebd0a5736fdfc3c2dc23684bc82cdc7a507a01
shima-kumiko <egoistkumi@gmail.com>
2015-09-08 03:03:01 +0900

    修正

--- lifeGame/main.js
+++ lifeGame/main.js
@@ -1,42 +1,44 @@
 /* グローバル変数の宣言 */
 
-var SCREEN_SIZE = 640;
-var SIDE_CELLS = 200;
+var SCREEN_SIZE = 500;
+var SIDE_CELLS = 150;
 var CELL_SIZE = SCREEN_SIZE / SIDE_CELLS;
 var FPS = 10;
 var canvas;
 var context;
 
 window.onload  = function() {
-	var fiele = new Array(SIDE_CELLS*SIDE_CELLS);
+	var field = new Array(SIDE_CELLS*SIDE_CELLS);
 	var tempField = new Array(SIDE_CELLS*SIDE_CELLS);
-	for (var i=0; i<field.length; i++)　field[i] = Math.floor(Math.random()*2)
+	for (var i=0; i<field.length; i++)　field[i] = Math.floor(Math.random()*2);
 
 	canvas = document.getElementById('world');
 	canvas.width = canvas.height = SCREEN_SIZE;
 	var scaleRate = Math.min(window.innerWidth/SCREEN_SIZE, window.innerHeight/SCREEN_SIZE);
 	canvas.style.width = canvas.style.height = SCREEN_SIZE*scaleRate+'px';
 	context = canvas.getContext('2d');
-	context.fillStyle = 'rgb(211, 85 149)';
+	context.fillStyle = 'rgb(211, 85, 149)';
 	update(field, tempField);
 	
 }
 
-function update() {
+function update(field, tempField) {
 	var n = 0;
 	tempField = field.slice();
-	for (var i=0; i<tempField.length; i++){
+	for (var i=0; i<tempField.length; i++) {
 		n = 0;
-		for (var s=-1; s<2; s++){
-			if (s==0 && t==0) continue;
-			var c =i+s*SIDE_CELLS+t;
-			if (c>=0 && c<tempField.length) {
-				if (<c && c%SIDE_CELLS!=0 || i>c && c%SIDE_CELLS!=SIDE_CELLS-1) {
-					if (tempField[c]) n ++
+		for (var s=-1; s<2; s++) {
+			for (var t=-1; t<2; t++) {
+				if (s==0 && t==0) continue;
+				var c = i+s*SIDE_CELLS+t;
+				if (c>=0 && c<tempField.length) {
+					if (i<c && c%SIDE_CELLS!=0 || i>c && c%SIDE_CELLS!=SIDE_CELLS-1) {
+						if (tempField[c]) n++;
+					}
 				}
 			}
 		}
-		if (tempField[i] && (n--2||n==3)) {
+		if (tempField[i] && (n==2||n==3)) {
 			 field[i] = 1;
 		} else if (!tempField[i] && n==3) {
 			field[i] = 1;
@@ -46,7 +48,7 @@ function update() {
 	setTimeout(update, 1000/FPS, field, tempField); 
 }
 
-function draw() {
+function draw(field) {
 	 context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE); // 画面をクリア
     for (var i=0; i<field.length; i++) {
         var x = (i%SIDE_CELLS) * CELL_SIZE;             // x座標
